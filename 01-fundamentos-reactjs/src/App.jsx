c8a327be8e651aa6589f8b22e9eeb8489906161c
Andressa Da Costa <andressadacostan@gmail.com>
2022-07-12 21:52:45 -0300

    components

--- 01-fundamentos-reactjs/src/App.jsx
+++ 01-fundamentos-reactjs/src/App.jsx
@@ -1,9 +1,65 @@
-function App() {
+// ****** Named Exports
 
+import { Post } from "./Post";
+
+export function App() {
   return (
-                     
-    <h1>Hello World</h1>
+  <div>
+    <Post/>
+    <Post/>
+    <Post/>
+    <Post/>
+    <Post/>
+  </div>  
+  )
+}
+
+
+//componente: funçao que retorna html 
+
+// JSX = Javascript + XML
+
+
+// *******Defaut Exports*********
+
+// import Post from "./Post";
+
+// function App() {
+//   return (
+//   <div>
+//     <Post/>
+//     <Post/>
+//     <Post/>
+//     <Post/>
+//     <Post/>
+//   </div>  
+//   )
+// }
+
+// export default App
+
+
+
+
+/*import PostComponent from "./Post";
+
+function App() {
+  return (
+  <div>
+    <PostComponent/>
+    <PostComponent/>
+    <PostComponent/>
+    <PostComponent/>
+    <PostComponent/>
+     </div>  
   )
 }
 
 export default App
+
+------- function Post() {
+  return 
+  <p> Post</p>
+}
+
+export default Post */
